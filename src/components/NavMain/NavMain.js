8dbe7e049e7666aa0b3183bec2830c60f84add0a
Gustav <gustaveen@gmail.com>
2018-07-12 14:18:42 +0200

    Set active class on Link

--- src/components/NavMain/NavMain.js
+++ src/components/NavMain/NavMain.js
@@ -1,3 +1,4 @@
+/* global location */
 import React from 'react'
 import Link from 'gatsby-link'
 import { FormattedMessage } from 'react-intl';
@@ -6,48 +7,40 @@ import SelectLanguage from '../SelectLanguage';
 
 const NavMain = (props) => (
     <ul className="NavMain">
-        <li
-            className={
-            location.pathname === `/${props.currentLang}/rooms/`
-                ? 'NavMain__Item NavMain__Item--Active'
-                : 'NavMain__Item'
-            }
-        >
-            <Link to={`/${props.currentLang}/rooms/`} className="NavMain__ItemLink">
+        <li className="NavMain__Item">
+            <Link 
+                to={`/${props.currentLang}/rooms/`} 
+                className="NavMain__ItemLink"
+                activeClassName="NavMain__ItemLink--Active" 
+            >
                 <FormattedMessage id="navRooms" />
             </Link>
         </li>
-        <li
-            className={
-            location.pathname === `/${props.currentLang}/activities/`
-                ? 'NavMain__Item NavMain__Item--Active'
-                : 'NavMain__Item'
-            }
-        >
-            <Link to={`/${props.currentLang}/activities/`} className="NavMain__ItemLink">
-                <FormattedMessage id="navActivities" />
+        <li className="NavMain__Item">
+            <Link 
+                to={`/${props.currentLang}/activities/`} 
+                className="NavMain__ItemLink"
+                activeClassName="NavMain__ItemLink--Active"
+            >
+                <FormattedMessage id="navActivities"/>
             </Link>
         </li>
-        <li
-            className={
-            location.pathname === '/rooms/'
-                ? 'NavMain__Item--Active'
-                : 'NavMain__Item'
-            }
-        >
-            <Link to={`/${props.currentLang}/rooms/`} className="NavMain__ItemLink">
-                <FormattedMessage id="navPackages" />
+        <li className="NavMain__Item">
+            <Link 
+                to={`/${props.currentLang}/packages/`} 
+                className="NavMain__ItemLink"
+                activeClassName="NavMain__ItemLink--Active"
+            >
+                <FormattedMessage id="navPackages"/>
             </Link>
         </li>
-        <li
-            className={
-            location.pathname === '/rooms/'
-                ? 'NavMain__Item--Active'
-                : 'NavMain__Item'
-            }
-        >
-            <Link to={`/${props.currentLang}/rooms/`} className="NavMain__ItemLink">
-                <FormattedMessage id="navFindUs" />
+        <li className="NavMain__Item">
+            <Link 
+                to={`/${props.currentLang}/find-us/`} 
+                className="NavMain__ItemLink"
+                activeClassName="NavMain__ItemLink--Active"
+            >
+                <FormattedMessage id="navFindUs"/>
             </Link>
         </li>
     </ul>
